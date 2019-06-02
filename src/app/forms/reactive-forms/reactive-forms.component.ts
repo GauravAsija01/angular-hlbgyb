0e67bdc2d0ab96fdbf2230f0137056abd208a808
GauravAsija01 <gaurav.designer01@gmail.com>
2019-06-02 05:32:06 -0700

    Reactive form with validation Complete Part-1

--- src/app/forms/reactive-forms/reactive-forms.component.ts
+++ src/app/forms/reactive-forms/reactive-forms.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
+import { MustMatch } from './ps-match.validator';
 
 @Component({
   selector: 'app-reactive-forms',
@@ -14,32 +15,59 @@ export class ReactiveFormsComponent implements OnInit {
   ngOnInit() {
 
     this.fbGroupForm();
+    //this.formGroupType();
+    // this.registration.valueChanges.subscribe(data => {
+    //   console.log(data);
+    // });
   }
 
 
   fbGroupForm(){
+    const passwordPattern = "^[a-z0-9_-]{8,15}$"; 
     this.registration = this.fb.group({
       firstname: ["Gaurav", [Validators.required, Validators.minLength(3)]],
       lastname: ["Asija"],
-      emailid: ["gaurav.designer01@gmail.com"],
+      emailid: ["gaurav.designer01@gmail.com", [Validators.required, Validators.email]],
       newPassword: this.fb.group({
-       npassword: ["gaur1234", [Validators.required]], //Validators.pattern('^(?=.*\d).{4,8}$')]
-       cpassword: ["gaur1234", [Validators.required]]
-      })
-
-      // password: this.fb.group({
-      //    npassword: ["gaur1234"],
-      //    cpassword: ["gaur1234"]
-      //   })
-
+       npassword: ["gaur1234", [Validators.required, Validators.pattern(passwordPattern)]],
+       cpassword: ["gaur1234", [Validators.required, Validators.pattern(passwordPattern)]]
+      }, {
+        validator: MustMatch('npassword', 'cpassword')
+    })
     });
   }
 
+  
+
+  // formGroupType(){
+  //   const passwordPattern = "^[a-z0-9_-]{8,15}$"; 
+  //   this.registration = new FormGroup({
+  //     firstname: new FormControl("Gaurav", [Validators.required, Validators.minLength(3)]),
+  //     lastname: new FormControl("Asija"),
+  //     emailid: new FormControl("gaurav.designer01@gmail.com", [Validators.required, Validators.email]),
+  //     newPassword: new FormGroup({
+  //       npassword: new FormControl("gaur1234", [Validators.required, Validators.pattern(passwordPattern)]),
+  //       cpassword: new FormControl("gaur1234", [Validators.required, Validators.pattern(passwordPattern)])
+  //     }, {
+      //   validator: MustMatch('npassword', 'cpassword')
+      // })
+  //   });
+  // }
+
+  get form() { return this.registration.controls; }
 
 
   submitForm(){
-    console.log(this.registration.value);
+    if(this.registration.valid){
+      console.log(this.registration);
+      alert("Form Submitted!");
+    }
+    //console.log(this.registration.value);
     //console.log(this.registration.getRawValue());
+  }
+
+  resetForm(){
+    this.registration.reset();
   }
 
 }
